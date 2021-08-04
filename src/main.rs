ab1fce65098233a598fa1c4e4efea9662a9f0ba2
rr-rr-re <rr.anonymous.2020@gmail.com>
2021-08-04 19:07:27 +0900

    simple

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
+
 use tokio::{
-    io::{AsyncReadExt, AsyncWriteExt},
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
     net::TcpListener
 };
 
@@ -7,14 +8,22 @@ use tokio::{
 async fn main() {
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
 
-    let (mut socket, _addr) = listener.accept().await.unwrap();
-
     loop {
-        let mut buffer = [0u8; 1024];
+        let (mut socket, _addr) = listener.accept().await.unwrap();
+        
+        let (reader, mut writer) = socket.split();
 
-        let bytes_read = socket.read(&mut buffer).await.unwrap();
+        let mut reader = BufReader::new(reader);
+        let mut line = String::new();
 
-        socket.write_all(&buffer[..bytes_read]).await.unwrap();
+        loop {
+            let bytes_read = reader.read_line(&mut line).await.unwrap();
+            if bytes_read == 0 {
+                break;
+            }
+            writer.write_all(line.as_bytes()).await.unwrap();
+            line.clear();
+        }
     }
 }
   
